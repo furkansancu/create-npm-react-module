745f145eb3f1c00ca5f17d8d976aa048d466ce2f
furkansancu <furkansancu@outlook.com>
2023-03-11 06:50:23 +0300

    linux path error fixed

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
+import path from 'path';
+
 import packageMenager from './components/package-menager';
 import consoleMenager from './components/console-menager';
 import diskMenager from './components/disk-menager';
 import shellMenager from './components/shell-menager';
 
 async function createProject () {
-    const projectName = process.argv[2]
-    const projectPath = `${process.cwd()}\\${projectName}\\`;
+    const projectName = process.argv[2];
+    const projectPath = path.join(process.cwd(), projectName);
     const options = process.argv.slice(3, process.argv.length);
     const packageCommands = await packageMenager.decide(options);
 
